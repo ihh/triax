cf7f5e946812df1833e13f79171ef6f5e893dcab
Ian Holmes <ihholmes@gmail.com>
2021-02-14 14:40:31 -0800

    First card

---
+++ js/triax.js
@@ -0,0 +1,2 @@
+const cards = [{ html: 'You are offered an angel investment from Peter Thiel. Do you want to accept it?', limit: 1, left: { hint: 'No' }, right: { hint: 'Yes', reward: { coins: 20, evil: 30 } } },
+	       { html: 'Game Over', priority: -1 }]
